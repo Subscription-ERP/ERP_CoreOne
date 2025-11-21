43d62fe10f87233881847962272939d918bb951a
jaewoo bang <jaewoo@gamil.com>
2025-11-21 15:36:25 +0900

    ERP Project Start

---
+++ src/main/java/com/rootcore/Application.java
@@ -0,0 +1,15 @@
+package com.rootcore;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan(basePackages = "com.rootcore.**.mapper")
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/rootcore/common/mapper/SampleMapper.java
@@ -0,0 +1,8 @@
+package com.rootcore.common.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SampleMapper {
+	public String test();
+}
