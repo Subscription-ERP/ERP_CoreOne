b5423f4fc545c242d7bf04907e748de7cbc37a7c
YUHAN KO <rhdbgks789@gmail.com>
2025-11-21 17:55:29 +0900

    [KYH]초기커밋

---
+++ src/main/java/com/rootcore/tax/controller/TaxController.java
@@ -0,0 +1,15 @@
+package com.rootcore.tax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaxController {
+
+	@GetMapping("/tax/invoice")
+	public String taxInvoice() {
+	    return "tax/taxInvoice";
+	}
+
+}
