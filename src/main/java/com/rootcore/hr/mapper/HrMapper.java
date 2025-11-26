74fdc67babb6099bbb3172f194ab75bcb7971872
junhyeon0325 <jjh990325@gmail.com>
2025-11-26 17:20:37 +0900

    [JJH] 251126 급여대장 사원조회, 급여조회 완료

--- src/main/java/com/rootcore/hr/mapper/HrMapper.java
+++ src/main/java/com/rootcore/hr/mapper/HrMapper.java
@@ -2,6 +2,7 @@ package com.rootcore.hr.mapper;
 
 import java.util.List;
 
+import com.rootcore.hr.vo.PayrollVO;
 import com.rootcore.hr.vo.UserVO;
 
 public interface HrMapper {
@@ -10,6 +11,8 @@ public interface HrMapper {
 	//=================
 	// 급여대장-상여등록-사원조회
 	List<UserVO> selectUserList(UserVO param);
+	// 급여대장-조회
+	List<PayrollVO> selectPayrollList(PayrollVO param);
 	
 	//=================
 	// 이한솔
