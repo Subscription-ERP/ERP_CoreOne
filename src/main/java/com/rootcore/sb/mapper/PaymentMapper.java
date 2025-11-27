668391ef848a0068137505184f06c5548a050380
방재우 <wodn2365@gmail.com>
2025-11-28 00:09:00 +0900

    BJW_251128 결제 API 서버 수정

--- src/main/java/com/rootcore/sb/mapper/PaymentMapper.java
+++ src/main/java/com/rootcore/sb/mapper/PaymentMapper.java
@@ -2,12 +2,12 @@ package com.rootcore.sb.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import com.rootcore.sb.domain.Payment;
+import com.rootcore.sb.vo.ExistSubsPaymentVO;
+import com.rootcore.sb.vo.NewSubsPaymentVO;
 
 @Mapper
 public interface PaymentMapper {
 
-    void insertPayment(Payment payment); // 결제 승인 시 INSERT
-
-    // 필요하다면 이 정도만
-    Payment selectLatestByOrderId(String orderId);
+	void callNewSubsAndPayment(NewSubsPaymentVO vo);
+    void callExistSubsPayment(ExistSubsPaymentVO vo);
 }
