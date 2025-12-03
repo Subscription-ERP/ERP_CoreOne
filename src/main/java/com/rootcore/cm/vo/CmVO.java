e1a663e03ec64e7fab9e7e9a64ddfae0bd9963db
bong <p1-02@naver.com>
2025-12-03 17:20:44 +0900

    [PBG] 251203 영업 수주 등록, 품목 조회 작업 중

--- src/main/java/com/rootcore/cm/vo/CmVO.java
+++
@@ -1,4 +0,0 @@
-package com.rootcore.cm.vo;
-
-public class CmVO {
-}
---
+++ src/main/java/com/rootcore/cm/vo/SkuVO.java
@@ -0,0 +1,13 @@
+package com.rootcore.cm.vo;
+
+import lombok.Data;
+
+@Data
+public class SkuVO {
+    private String sku;
+    private String skuName;
+    private String skuType;
+    private String spec;
+    private String unit;
+    private String remark;
+}
