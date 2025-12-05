d35880b324c8f120829b0bb6edba2ab92601a91f
bong <p1-02@naver.com>
2025-12-05 17:18:49 +0900

    [PBG] 251203 공통 품목 조회 수정

--- src/main/java/com/rootcore/cm/vo/SkuVO.java
+++ src/main/java/com/rootcore/cm/vo/SkuVO.java
@@ -2,15 +2,21 @@ package com.rootcore.cm.vo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SkuVO {
-    private String sku;         // 품목코드
-    private String skuName;     // 품목명
-    private String skuType;     // 품목타입
-    private String spec;        // 규격
-    private String unit;        // 단위
-    private String remark;      // 비고
-    private String unitPriceType;   // 단가유형코드
-    private String typeName;        // 단가유형이름
-    private int unitPrice;          // 단가
+    private String companyCode;     // 회사코드
+    private String sku;             // 품목코드
+    private String skuName;         // 품목명
+    private String skuType;         // 품목분류
+    private String spec;            // 규격
+    private String unit;            // 단위
+    private String remark;          // 비고
+    private String useYn;           // 사용여부
+    private String taxYn;           // 과세여부
+    private String createdBy;       // 생성자
+    private Date createDate;        // 생성일자
+    private String updatedBy;       // 수정자
+    private Date updateDate;        // 수정일자
 }
