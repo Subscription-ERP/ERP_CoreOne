b8a269daf40709bdfa9911911387396792e3a76a
junhyeon0325 <jjh990325@gmail.com>
2025-12-08 17:20:00 +0900

    [JJH] 251208 연차관리HTML작성, 연차신청내역조회, 내연차현황조회, 연차등록FORM작성,
    급여대장PDF출력 머리말 수정, 연차등록구현중

---
+++ src/main/java/com/rootcore/hr/mapper/AnnualManageMapper.java
@@ -0,0 +1,19 @@
+package com.rootcore.hr.mapper;
+
+import java.util.List;
+
+import com.rootcore.hr.vo.AnnualLeaveDetailVO;
+import com.rootcore.hr.vo.AnnualLeaveVO;
+
+public interface AnnualManageMapper {
+
+	// 사원연차신청이력조회
+	List<AnnualLeaveDetailVO> selectAnnualManageList(AnnualLeaveDetailVO param);
+	
+	// 연차현황
+	AnnualLeaveVO selectmyAnnualStatus(AnnualLeaveVO param);
+
+	// 연차신청
+	int insertmyAnnualApply(AnnualLeaveDetailVO param);
+
+}
---
+++ src/main/java/com/rootcore/hr/vo/AnnualLeaveDetailVO.java
@@ -0,0 +1,25 @@
+package com.rootcore.hr.vo;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class AnnualLeaveDetailVO {
+	// 사원 기본 정보
+	private String userId; // 사번
+	private String userName; // 성명
+	private String deptName; // 부서명
+	private String jobTitle; // 직위직급 (job_title)
+
+	// 연차 사용 상세 정보 (TB_ANNUAL_LEAVE_DETAIL 연계)
+	private String leaveType; // 신청구분 (leave_type)
+	private double usedDays; // 사용일수 (used_days)
+	private Date leaveStartDate; // 연차시작일 (leave_start_date)
+	private Date leaveEndDate; // 연차종료일 (leave_end_date)
+	private Date leaveApplyDate; // 연차신청일 (leave_apply_date)
+	private String rm; // 사유 (rm)
+	
+	private String annualLeaveCode;  // 연차관리코드
+	private Long annualLeaveDetailSeq; // 연차상세관리번호
+}
