1853257cc9e58346681a63cc85a157a497a0e65a
HANSOL LEE <hansol2886@gmail.com>
2025-12-19 17:38:19 +0900

    [LHS] 251219 공통 레이아웃(상단네비 정리, breadcrumb)
    - 로그인 사원 session 쿼리/vo/수정 (공통코드 및 부서, 사원이미지 추가)
    - 사원이미지 연결
    - fontawesome 아이콘으로 변경
    - breadcrumb 작성 및 controller수정
    - 로그인페이지 button색상 수정(공통 스타일)

--- src/main/java/com/rootcore/hr/web/HrController.java
+++ src/main/java/com/rootcore/hr/web/HrController.java
@@ -19,24 +19,40 @@ public class HrController {
 	
 	// 사원관리
 	@GetMapping("/hr/userManage")
-	public String userManage() {
+	public String userManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "사원관리"
+		    ));
 		return "hr/userManage";
 	}
 	// 조직도 관리
 	@GetMapping("/hr/deptManage")
-	public String deptManage() {
+	public String deptManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "조직도 관리"
+		    ));
 		return "hr/deptManage";
 	}
 	
 	// 연차
 	// 연차관리
 	@GetMapping("/hr/annualManage")
-	public String annaulManage() {
+	public String annaulManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "연차관리"
+		    ));
 		return "hr/annual/annualManage";
 	}
 	// 연차현황
 	@GetMapping("/hr/annualStatus")
-	public String annualStatus() {
+	public String annualStatus(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "연차현황조회"
+		    ));
 		return "hr/annual/annualStatus";
 	}
 	
@@ -44,6 +60,11 @@ public class HrController {
 	// 근태관리(관리자)
 	@GetMapping("/hr/attendanceManage")
 	public String attendanceManage(Model model) {
+		
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "근태관리"
+		    )); 
 		
 		List<Map<String, String>> attendTypes = List.of(
 		        Map.of("type", "h1", "label", "정상", "color", "#A0A0A0"),
@@ -64,35 +85,59 @@ public class HrController {
 	}
 	// 내근태관리
 	@GetMapping("/hr/attendanceMe")
-	public String attendanceMe() {
+	public String attendanceMe(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "내 근태관리"
+		    ));
 		return "hr/attendance/attendanceMe";
 	}
 	
 	// 급여	
 	// 급여 관리(조회) 페이지 이동
 	@GetMapping("/hr/payrollManage")
-	public String payrollManage() {
+	public String payrollManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "급여조회"
+		    ));
 		return "hr/payroll/payrollManage";
 	}
 	// 급여 대장 페이지 이동
 	@GetMapping("/hr/payrollReport")
-	public String payrollReport() {
+	public String payrollReport(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "급여대장"
+		    ));
 		return "hr/payroll/payrollReport";
 	}
 	
 	// 인사평가	
 	@GetMapping("/hr/reviewMaster")
-	public String reviewMaster() {
+	public String reviewMaster(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "인사평가 기준관리"
+		    ));
 		return "hr/review/reviewMaster";
 	}
 	@GetMapping("/hr/reviewManage")
-	public String reviewManage() {
+	public String reviewManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "인사평가관리"
+		    ));
 		return "hr/review/reviewManage";
 	}
 	
 	// 증명서 
 	@GetMapping("/hr/certificationManage")
-	public String certificationManage() {
+	public String certificationManage(Model model) {
+		model.addAttribute("breadcrumb", List.of(
+		        "인사",
+		        "증명서관리"
+		    ));
 		return "hr/certificationManage";
 	}
 	
