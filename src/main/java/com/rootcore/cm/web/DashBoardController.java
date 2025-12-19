f3e4d46e1f3396e634b1d4775e66f5633ea4fa92
junhyeon0325 <jjh990325@gmail.com>
2025-12-19 17:53:44 +0900

    [JJH] 251219 DASHBOARD 작업

---
+++ src/main/java/com/rootcore/cm/web/DashBoardController.java
@@ -0,0 +1,18 @@
+package com.rootcore.cm.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rootcore.cm.service.DeptCodeManageService;
+
+import lombok.RequiredArgsConstructor;
+
+/**---------------------
+ * 메인대쉬보드 컨트롤러
+ * ---------------------*/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cm")
+public class DashBoardController {
+	
+}
